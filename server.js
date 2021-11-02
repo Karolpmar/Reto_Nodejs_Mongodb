80ed2155bb56f18ce2381e199d7c6943c99725a6
Karolpmar <karol.pino.m@gmail.com>
2021-11-01 22:30:10 -0500

    Creación carpeta Modelos y archivo Usuario.js

--- server.js
+++ server.js
@@ -11,4 +11,14 @@ app.use(express.json());
 
 mongoose.connect("mongodb+srv://KarolPino:Sophia0518@clusterretonodejsmongod.fyc4h.mongodb.net/test")
 
+//Operaciones CRUD
+
+app.use(router);
+app.listen(3000, () => {
+    console.log("Servidor corriendo en el puerto 3000")
+});
+
+router.get('/',(req, res) => {
+    res.send("Este es el inicio de mi primera API");
+});
 
